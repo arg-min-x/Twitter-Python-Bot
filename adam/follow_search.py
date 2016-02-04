3a471809c243e89699950751887179ff70d023c3
arg-min-x <rich.178@osu.edu>
2016-02-04 00:34:00 -0500

    more mysql stuff

--- adam/follow_search.py
+++ adam/follow_search.py
@@ -9,7 +9,6 @@ warnings.filterwarnings("ignore")
 twitter = twitterTools()
 twitter.authenticate('access_key.csv')
 
-
 # Open database connection
 db = MySQLdb.connect("localhost","root","","twitterTest" )
 
@@ -17,24 +16,19 @@ db = MySQLdb.connect("localhost","root","","twitterTest" )
 cursor = db.cursor()
 
 #for friend in tweepy.Cursor(twitter.api.friends,screen_name = 'CLCShareCloud').items(1):
-for friend in tweepy.Cursor(twitter.api.friends).items():
+##for friend in tweepy.Cursor(twitter.api.followers, screen_name = 'taylorswift13').items():
+for friend in tweepy.Cursor(twitter.api.followers).items():
 
-    # Prepare SQL query to INSERT a record into the database.
-    sql = twitter.userToMysql(friend)
+    # check if user ID is already in the databse
+    userInDatabase = twitter.isInDatabase(friend.id,db)
+    if userInDatabase:
+        print "user already in database"
+    else:
+        # Prepare SQL query to INSERT a record into the database.
+        twitter.userToMysql(friend,db)
 
-    try:
-       # Execute the SQL command
-       cursor.execute(sql)
-       # Commit your changes in the database
-       db.commit()
-    except:
-       # Rollback in case there is any error
-       db.rollback()    # Execute the SQL command
-       print 'f up'
-       print sql
-
-    print '.'
-    #time.sleep(6)
+    #print '.'
+    time.sleep(5.1)
         
 ## disconnect from server
 db.close()
--- adam/twitterTools.py
+++ adam/twitterTools.py
@@ -9,6 +9,7 @@ class twitterTools:
         self.ACCESS_TOKEN = ''
         self.ACCESS_TOKEN_SECRET = ''
         self.api = 0
+        self.database = []
         
     # authenticate to the twitter account with credentials stored in the csv
     # file name "fileName"
@@ -26,8 +27,8 @@ class twitterTools:
         auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         self.api = tweepy.API(auth)
 
-    # Convert user information into a string for addition to MySQL database
-    def userToMysql(self,user):
+    # Convert user information into a string and insert into MySQL database
+    def userToMysql(self,user,database):
         # Prepare SQL string to INSERT a record into the database.
         user.description = user.description.replace("\'", "\\\'")
         user.location = user.location.replace("\'", "\\\'")
@@ -38,10 +39,58 @@ class twitterTools:
         user.follow_request_sent = int(user.follow_request_sent)
         user.created_at = user.created_at.isoformat()
         user.created_at = user.created_at.replace('T',' ')
+        if user.time_zone:
+            user.time_zone = user.time_zone.replace("\'", "\\\'")
 
         dateCrawled = strftime("%Y-%m-%d %H:%M:%S", localtime())
         
-        mysqlString = "INSERT INTO user(screenName,name,ID, description, location, following, friendsCount, followersCount, favouritesCount, defaultProfile, verified, followRequestSent, dateCreated, dateCrawled) VALUES ('%s', '%s', %d, '%s', '%s', %d, %d, %d, %d, %d, %d, %d, '%s', '%s');"\
-          % (user.screen_name, user.name, user.id, user.description, user.location, user.following, user.friends_count,user.followers_count,user.favourites_count, user.default_profile, user.verified, user.follow_request_sent, user.created_at, dateCrawled)
+        mysqlString = ("INSERT INTO user2(screenName,name,ID, description, location,"
+                       "following, friendsCount, followersCount, favouritesCount, "
+                       "defaultProfile, verified, followRequestSent, timezone, "
+                       "dateCreated, dateCrawled) VALUES ('%s', '%s', %d, '%s', "
+                       "'%s', %d, %d, %d, %d, %d, %d, %d, '%s', '%s', '%s');")\
+                       % (user.screen_name, user.name, user.id, user.description, \
+                          user.location, user.following, user.friends_count,\
+                          user.followers_count,user.favourites_count, user.default_profile,\
+                          user.verified, user.follow_request_sent, user.time_zone,\
+                          user.created_at, dateCrawled)
         mysqlString = mysqlString.encode('ascii',errors='ignore')
-        return mysqlString
+
+        # prepare a cursor object using cursor() method
+        cursor = database.cursor()
+        try:
+           # Execute the SQL command
+           cursor.execute(mysqlString)
+           # Commit your changes in the database
+           database.commit()
+        except:
+           # Rollback in case there is any error
+           database.rollback()
+           print mysqlString
+
+    # Check if a user is already in the database
+    def isInDatabase(self,ID,database):
+        
+        # prepare a cursor object using cursor() method
+        cursor = database.cursor()
+
+        # Prepare SQL query to INSERT a record into the database.
+        mysqlString = "SELECT * FROM user2 \
+               WHERE ID LIKE %d" %(ID)
+        try:
+            # Execute the SQL command
+            cursor.execute(mysqlString)
+            # Fetch all the rows in a list of lists.
+            results = cursor.fetchall()
+    
+            if results:
+                inDatabase = 1
+            else:
+                inDatabase = 0
+                
+        except:
+            print "Error: unable to fecth data"
+            inDatabase = 1
+        
+        return inDatabase
+
