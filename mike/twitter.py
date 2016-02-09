abc9b6204fbbca73ceda546fa076cde9d134d854
mriedl7 <michaelriedl@Michaels-Air-2.columbus.rr.com>
2016-02-08 19:48:56 -0500

    New folder
    Created a new folder for mike’s code

---
+++ mike/twitter.py
@@ -0,0 +1,16 @@
+import time
+import warnings
+from twitterTools import *
+
+# supress all warnings
+warnings.filterwarnings("ignore")
+
+twitter = twitterTools()
+twitter.authenticate('access_key.csv')
+
+
+        
+
+
+
+
